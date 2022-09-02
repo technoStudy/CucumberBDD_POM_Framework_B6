8f20c7ad548c880e9023ac55b7204e013fe65d61
hmk10 <halitmk@gmail.com>
2022-09-01 20:21:08 -0400

    Cucumber BDD - POM Framework | Step Definition - Countries Function

--- src/test/java/StepDefinition/CountriesSteps.java
+++ src/test/java/StepDefinition/CountriesSteps.java
@@ -1,17 +1,28 @@
 package StepDefinition;
 
+import POM.DialogContentElements;
+import POM.NavigationBarElements;
 import io.cucumber.java.en.*;
 
 public class CountriesSteps {
 
+    NavigationBarElements navigationBarElements;
+    DialogContentElements dialogContentElements;
+
     @When("User create Country")
     public void user_create_country() {
+        navigationBarElements = new NavigationBarElements();
+        dialogContentElements = new DialogContentElements();
 
+        navigationBarElements.navigateToCountriesPage();
+        dialogContentElements.createCountry();
     }
 
     @Then("Success message should be displayed")
     public void success_message_should_be_displayed() {
+        dialogContentElements = new DialogContentElements();
 
+        dialogContentElements.validateSuccessMessage();
     }
 
 }
