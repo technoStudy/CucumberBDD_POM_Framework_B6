212398befa416a85f2815ca94145aa369438727c
hmk10 <halitmk@gmail.com>
2022-09-01 20:20:51 -0400

    Cucumber BDD - POM Framework | Page Classes

--- src/test/java/POM/DialogContentElements.java
+++ src/test/java/POM/DialogContentElements.java
@@ -1,11 +1,44 @@
 package POM;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 public class DialogContentElements extends BasePOM {
 
     public DialogContentElements() {
+
         PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//ms-add-button[contains(@tooltip,'TITLE.ADD')]//button")
+    private WebElement addButton;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='name']//input")
+    private WebElement nameInput;
+
+    @FindBy(xpath = "//ms-text-field[@formcontrolname='code']//input")
+    private WebElement codeInput;
+
+    @FindBy(xpath = "//ms-save-button//button")
+    private WebElement saveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    private WebElement successMessage;
+
+    public void createCountry() {
+        addButton.click();
+        nameInput.sendKeys("HalitTestCountry999");
+        codeInput.sendKeys("HTC999");
+        saveButton.click();
+    }
+
+    public void validateSuccessMessage() {
+        wait.until(ExpectedConditions.visibilityOf(successMessage));
+        Assert.assertTrue(successMessage.isDisplayed());
+        Assert.assertTrue(successMessage.getText().toLowerCase().contains("successfully"));
     }
 
 }
--- src/test/java/POM/NavigationBarElements.java
+++ src/test/java/POM/NavigationBarElements.java
@@ -1,11 +1,28 @@
 package POM;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class NavigationBarElements extends BasePOM {
 
     public NavigationBarElements() {
         PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//span[text()='Setup']/..")
+    private WebElement setupButton;
+
+    @FindBy(xpath = "//span[text()='Parameters']/..")
+    private WebElement parametersButton;
+
+    @FindBy(xpath = "//span[text()='Countries']/..")
+    private WebElement countriesButton;
+
+    public void navigateToCountriesPage() {
+        setupButton.click();
+        parametersButton.click();
+        countriesButton.click();
     }
 
 }
