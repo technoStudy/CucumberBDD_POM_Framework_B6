03a01992be8e908a9c10106fd0a7deb2339ac52f
hmk10 <halitmk@gmail.com>
2022-09-06 21:11:46 -0400

    Cucumber BDD - POM Framework | Screenshot

--- src/test/java/StepDefinition/Hooks.java
+++ src/test/java/StepDefinition/Hooks.java
@@ -3,8 +3,14 @@ package StepDefinition;
 import Utils.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
@@ -19,7 +25,25 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+
+            TakesScreenshot screenshot = ((TakesScreenshot) driver); // which driver to get a screenshot
+
+            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+
+            String scenarioName = scenario.getName();
+
+            File destinationFile = new File("target/FailedScreenshots/" + scenarioName + ".png");
+
+            try {
+                FileUtils.copyFile(srcFile, destinationFile);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
         Driver.quitDriver();
     }
 
